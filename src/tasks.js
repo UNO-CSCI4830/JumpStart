7492efb600bf2e77a4a4d3f311ccabfddaa7fc7b
fritz-03 <fritzphilippemerino15@gmail.com>
2024-10-21 12:15:58 -0500

    cloned seth's branch

--- src/tasks.js
+++ src/tasks.js
@@ -3,41 +3,50 @@
  * - I'm not comprehensive, just stuff thought on a whim
  *
  */
-export  const tasks = [{
-        id: 1,
-        name: "Build Search Demo",
-        desc: "construct search component that captures user entry and displays in Result component."
-    }, {
-        id: 2,
-        name: "Build Post Demo",
-        desc: "Just like search demo, post will capture user entry and display in Result component"
-    }, {
-        id: 3,
-        name: "Internal datastructure",
-        desc: "Figure out how best would be to represent search/post elements to be displayed on page"
-    }, {
-        id: 4,
-        name: "Data storage",
-        desc: "Configure database so web data can be stored, searched, and retrieved"
-    }, {
-        id: 5,
-        name: "Storing data",
-        desc: "Configure function to take provided data, package it, and save it to database"
-    }, {
-        id: 6,
-        name: "Functional search",
-        desc: "Build functional search behavior that can pull relevant data from database"
-    }, {
-        id: 7,
-        name: "Feedback feature",
-        desc: "Implement interactable button that incraments/decraments a feedback score for posts on our site."
-    }, {
-        id: 8,
-        name: "Email confirmation",
-        desc: "Implement feature where user can enter an email, and they will receive an automated email from our service."
-    }, {
-        id: 9,
-        name: "Authenticate emails",
-        desc: "Automated emails sent to provided email addresses will provide a randomly generates string that will be compared to user input, and upon match, verifies emails.
-    }
+export const tasks = [
+  {
+    id: 1,
+    name: "Build Search Demo",
+    desc: "construct search component that captures user entry and displays in Result component.",
+  },
+  {
+    id: 2,
+    name: "Build Post Demo",
+    desc: "Just like search demo, post will capture user entry and display in Result component",
+  },
+  {
+    id: 3,
+    name: "Internal datastructure",
+    desc: "Figure out how best would be to represent search/post elements to be displayed on page",
+  },
+  {
+    id: 4,
+    name: "Data storage",
+    desc: "Configure database so web data can be stored, searched, and retrieved",
+  },
+  {
+    id: 5,
+    name: "Storing data",
+    desc: "Configure function to take provided data, package it, and save it to database",
+  },
+  {
+    id: 6,
+    name: "Functional search",
+    desc: "Build functional search behavior that can pull relevant data from database",
+  },
+  {
+    id: 7,
+    name: "Feedback feature",
+    desc: "Implement interactable button that incraments/decraments a feedback score for posts on our site.",
+  },
+  {
+    id: 8,
+    name: "Email confirmation",
+    desc: "Implement feature where user can enter an email, and they will receive an automated email from our service.",
+  },
+  {
+    id: 9,
+    name: "Authenticate emails",
+    desc: "Automated emails sent to provided email addresses will provide a randomly generates string that will be compared to user input, and upon match, verifies emails.",
+  },
 ];
