704464269f2b079b77747a0b49b2f6e34b367917
fritz-03 <fritzphilippemerino15@gmail.com>
2024-11-01 12:49:14 -0500

    fixed some errors

--- src/frontend/components/pages/EventsPage.js
+++ src/frontend/components/pages/EventsPage.js
@@ -1,20 +1,12 @@
 import React from "react";
-<<<<<<< HEAD
 import EventsHeader from "../../components/events-page/Events";
 import EventsList from "../events-page/EventsList";
-=======
->>>>>>> e69af340451c6e97daef36bbaaca9c43e9cafce1
 
 export default function EventsPage() {
   return (
     <div>
-<<<<<<< HEAD
       <EventsHeader />
       <EventsList />
-=======
-      <h1>Welcome to the Events Page</h1>
-      <p>We're broke :P</p>
->>>>>>> e69af340451c6e97daef36bbaaca9c43e9cafce1
     </div>
   );
 }
--- src/frontend/components/pages/ResourcePage.js
+++ src/frontend/components/pages/ResourcePage.js
@@ -2,15 +2,10 @@ import React, { useState } from "react";
 import "../../styles/ResourcePage.css";
 import ResourceSearch from "../resources/ResourceSearch";
 import ResourceCategories from "../resources/ResourceCategories";
-<<<<<<< HEAD
 import ResourceHeader from "../resources/ResourceHeader";
 import ResourceList from "../resources/ResourceList";
 import ResourceSubmitModal from "../resources/ResourceSubmitModal";
 import SubmitButton from "../resources/ResourceSubmitButton";
-=======
-import ResourceCard from "../resources/ResourceCard";
-import ResourceSubmitModal from "../resources/ResourceSubmitModal";
->>>>>>> e69af340451c6e97daef36bbaaca9c43e9cafce1
 import { initialResources } from "../../utils/resources";
 
 export default function ResourcePage() {
@@ -31,49 +26,14 @@ export default function ResourcePage() {
 
   return (
     <div className="resource-page">
-<<<<<<< HEAD
       <ResourceHeader />
-=======
-      <h1>JumpStart</h1>
-      <p className="subtitle">
-        Explore our wide range of student resources to support
-        <br />
-        your academic and personal goals.
-      </p>
-
->>>>>>> e69af340451c6e97daef36bbaaca9c43e9cafce1
       <ResourceSearch searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <ResourceCategories
         activeCategory={activeCategory}
         setActiveCategory={setActiveCategory}
       />
-<<<<<<< HEAD
       <ResourceList resources={filteredResources} />
       <SubmitButton onClick={handleOpenModal} />
-=======
-
-      <div className="resources">
-        {filteredResources.map((resource) => (
-          <ResourceCard
-            key={resource.id}
-            title={resource.title}
-            description={resource.description}
-            link={resource.link}
-          />
-        ))}
-      </div>
-      <button className="submit-resource-btn" onClick={handleOpenModal}>
-        <svg
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-        >
-          <path d="M12 5v14M5 12h14" />
-        </svg>
-        Submit a Resource
-      </button>
->>>>>>> e69af340451c6e97daef36bbaaca9c43e9cafce1
       {isModalOpen && <ResourceSubmitModal onClose={handleCloseModal} />}
     </div>
   );
