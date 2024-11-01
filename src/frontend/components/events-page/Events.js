a75f7d3a4defd9f5471ecd72be0ac2c0897f4825
fritz-03 <fritzphilippemerino15@gmail.com>
2024-11-01 12:38:00 -0500

    added a web scraper for UNO calendar

---
+++ src/frontend/components/events-page/Events.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../styles/EventsList.css";
+
+const EventsHeader = () => {
+  return (
+    <>
+      <h1 className="h1-Events">Campus Events</h1>
+      <p className="p-Events">
+        Stay connected with that what's happening at UNO
+      </p>
+    </>
+  );
+};
+
+export default EventsHeader;
