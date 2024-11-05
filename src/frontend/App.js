a6ea258353ebdf7fb1e0957942e665555a0bdb78
Seth Johnson <sjohnson154@unomaha.edu>
2024-11-05 15:19:31 -0600

    trying to match Fritz

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+<<<<<<< HEAD
 import HomePage from "./components/pages/HomePage";
 import Advice from "./components/pages/AdvicePage";
 import EventsPage from "./components/pages/EventsPage";
 import ConnectPage from "./components/pages/ConnectPage";
 import ResourcePage from "./components/pages/ResourcePage";
+=======
+import Home from "./components/Home";
+import Advice from "./components/AdvicePage";
+import Donate from "./components/Donate";
+import Discord from "./components/Discord";
+import ResourcePage from "./components/ResourcePage";
+>>>>>>> 5539b6ec9 (trying to match Fritz)
 
 function App() {
   return (
@@ -14,12 +22,21 @@ function App() {
         <Header />
         <main>
           <Routes>
+<<<<<<< HEAD
             <Route path="/" element={<HomePage />} />
             <Route path="/Home" element={<HomePage />} />
             <Route path="/resources" element={<ResourcePage />} />
             <Route path="/advice" element={<Advice />} />
             <Route path="/events" element={<EventsPage />} />
             <Route path="/connect" element={<ConnectPage />} />
+=======
+            <Route path="/" element={<Home />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/resources" element={<ResourcePage />} />
+            <Route path="/advice" element={<Advice />} />
+            <Route path="/donate" element={<Donate />} />
+            <Route path="/discord" element={<Discord />} />
+>>>>>>> 5539b6ec9 (trying to match Fritz)
           </Routes>
         </main>
       </div>
---
+++ src/frontend/components/Discord.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ConnectPage() {
+  return (
+    <div>
+      <h1>Welcome to the Connect Page</h1>
+      <p>A link to our Discord if you have any questions.</p>
+    </div>
+  );
+}
