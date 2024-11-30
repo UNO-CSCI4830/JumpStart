75fa4297890e5f4bb0f14511f1c82a3f2eeb2f0d
djseth-81 <sethdjohnson81@gmail.com>
2024-11-30 11:57:52 -0600

    Added tag filtering to DB queries for AdvicePage

--- src/backend/utils.js
+++ src/backend/utils.js
@@ -6,8 +6,6 @@ module.exports = class Database {
     collName = ""; // Igualmente for colllection name
     payload = []; // Where I'm expecting post data to go. Dunno if I wanna seperate incoming/outgoing payloads but that seems like a good idea
     errorStack = null; // How I'm currently passing along error codes from MongoDB to other sources for debugging atm
-    params = {}; // Query parameters
-    projs = {}; // Field filtering parameters unique to the find and findAll calls
 
     // Constructors
     constructor(dbname, collname) {
@@ -17,19 +15,26 @@ module.exports = class Database {
     }
 
     // Setters
-    async query() {
+    async pull(params = {}, projs = {}) {
+        console.log("DB instance: searching with params:");
+        console.log(params);
         try {
             console.log("Attempting to connect to DB...");
             await this.client.connect(); // Connect to MongoDB instance
             console.log(`Connected to DB!`);
             const queryColl = await this.client.db(this.dbName).collection(this.collName); // Connect to Collection to retrieve contents
-            this.payload = await queryColl.find(this.params,this.projs).toArray();
+            this.payload = await queryColl.find(params, projs).toArray();
         } catch (e) {
             this.errorStack = e;
             console.error(`An error occured when connecting ot DB:\n${e}`);
         } finally {
             this.client.close();
         }
+    }
+
+    async push(data) {
+    // TODO: write to DB
+    return 0;
     }
 
     // Getters
