c91698d18b416e4fbfc167077f6dd4b2f8efc8e9
djseth-81 <sethdjohnson81@gmail.com>
2024-12-03 17:48:29 -0600

    Trying to integrate error handling...

--- src/backend/utils.js
+++ src/backend/utils.js
@@ -6,10 +6,14 @@ module.exports = class Database {
     collName = ""; // Igualmente for colllection name
     payload = []; // Where I'm expecting post data to go. Dunno if I wanna seperate incoming/outgoing payloads but that seems like a good idea
     errorStack = null; // How I'm currently passing along error codes from MongoDB to other sources for debugging atm
+    retries = 3; // Maximum number of retry attemps for reading and writing
+    delay = 1; // Delay inbetween retries (counts in milliseconds)
 
     // Constructors
     constructor(dbname, collname) {
-        this.client = new MongoClient(this.uri);
+        this.client = new MongoClient(this.uri, {
+            serverSelectionTimeoutMS: 5000, //Time out for server - set for 5 seconds
+        });
         this.dbName = dbname;
         this.collName = collname;
     }
@@ -23,8 +27,12 @@ module.exports = class Database {
             await this.client.connect(); // Connect to MongoDB instance
             console.log(`Connected to DB!`);
             const queryColl = await this.client.db(this.dbName).collection(this.collName); // Connect to Collection to retrieve contents
-            const cursor = queryColl.find(params,projs); // since cursors reference objects...
+            // Enable a max of 3 requests to query database
+            // const cursor = this.retryOperation(() => queryColl.find(params,projs), maxRetries, retryDelay); // since cursors reference objects...
+            const cursor = queryColl.find(params,projs);
+
             this.payload = await cursor.toArray(); // and we want to make sure we get all objects
+
         } catch (e) {
             this.errorStack = e;
             console.error(`An error occured when retrieving posts from DB:\n${e}`);
@@ -37,6 +45,23 @@ module.exports = class Database {
     // TODO: write to DB
     return 0;
     }
+
+    // async retryOperation(operation) {
+    //     while (attempt < retries){
+    //         try{
+    //             return await operation();
+    //         } catch (error){
+    //             attempt++;
+    //             console.error(`Attempt ${attempt} failed: ${error.message}`);
+    //             if (attemtp >= retries){
+    //                 throw new Error(`Operation failed after ${retries} attempts: ${error.message}`);
+    //             }
+    //             console.log(`Retrying in ${delay}ms...`);
+    //             await new Promise(res => setTimeout(res, delay)); //wait befor trying again
+    //         }
+    //     }
+    // }
+
 
     // Getters
     getPayload() { return this.payload; }
