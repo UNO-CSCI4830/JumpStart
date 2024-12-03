66e2c173e2c5616d6db9474db04140dca4463b1d
TooAnonymous <131053442+TooAnonymous@users.noreply.github.com>
2024-12-02 21:21:37 -0600

    Unit testing for Assignment 3

--- src/backend/loadDB.js
+++ src/backend/loadDB.js
@@ -2,99 +2,109 @@
 // MongoDB Manual: https://www.mongodb.com/docs/manual/crud/
 // NodeJS Drivers Manual: https://www.mongodb.com/docs/drivers/node/current/fundamentals/crud/
 
-const resourcePosts = require("./resourcePosts.js").resourcePosts;
-const advicePosts = require("./advicePosts.js").advicePosts;
-const pendingPosts = require("./AdminData.js").pendingPosts;
+const resourcePosts = require("./data/resourcePosts.js").resourcePosts;
+const advicePosts = require("./data/advicePosts.js").advicePosts;
+const pendingPosts = require("./data/AdminData.js").pendingPosts;
+module.exports = { addEntries, getEntries, deleteEntries, lookEntries };
 
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
-async function listDBs(client) { // Lists all DBs in a MonboDB instance
-    let dbList = await client.db().admin().listDatabases();
+// Lists all DBs in a MongoDB instance
+async function listDBs(client) {
+    let dbList = await client.admin().listDatabases(); // Use admin() to access listDatabases
     dbList.databases.forEach(db => console.log(`${db.name}`));
     return dbList;
 }
 
-async function getEntries(collection, query={}, proj={}) { // retrieves all entries from a specified collection in a specified db
-    let entries = await collection.find(query).toArray();
-    // await entries.forEach(e => console.log(e));
-    return entries;
+
+// Retrieves all entries from a specified collection in a specified db
+async function getEntries(collection) {
+    try {
+        const entries = await collection.find().toArray();
+        return entries;
+    } catch (error) {
+        console.error('Error retrieving entries:', error);
+    }
 }
 
-async function addEntries(collection, arr=[]) { // takes elements of arr and adds them as entries into specified collection
-    console.log(await collection.insertMany(arr));
+// Takes elements of arr and adds them as entries into the specified collection
+async function addEntries(collection, arr = []) {
+    try {
+        const result = await collection.insertMany(arr);
+        console.log(`${result.insertedCount} entries added.`);
+    } catch (error) {
+        console.error('Error adding entries:', error);
+    }
+}
+
+// Deletes entries based on the query criteria
+async function deleteEntries(collection, query = {}) {
+    try {
+        const result = await collection.deleteMany(query);
+        console.log(`${result.deletedCount} entries deleted.`);
+        return result;
+    } catch (error) {
+        console.error('Error deleting entries:', error);
+    }
+}
+
+// Retrieves entries with specific keywords (search)
+async function lookEntries(collection, query) {
+    try {
+        const entries = await collection.find(query).toArray();
+        return entries;
+    } catch (error) {
+        console.error('Error retrieving entries:', error);
+    }
 }
 
 /* ========== MAIN ========== */
-async function configurePostsDB() {
 
+async function configurePostsDB() {
     // URI to connect to DB "Posts"
     const uri = "mongodb://localhost:27017/";
     const client = new MongoClient(uri); // MongoClient object with details to connect to Posts DB
 
     try { // attempt to establish a connection
         console.log(`Attempting connection to ${uri}...`);
-        await client.connect(); // sit and wait for DB to respond with "promise" object of our connection
-        /* Blocks all following execution until DB promise has been received. Think c-s demo from comm networks */
+        await client.connect(); // wait for DB to respond with "promise" object of our connection
         console.log(`Connected to ${uri}\n`);
 
         const postsDB = client.db("Posts");
 
+        // Listing databases (NOTE: Updated to correct function for MongoDB 4+)
         console.log(`DBs on ${uri}:`);
-        await listDBs(client);
 
-        // /* ### Building Resources collection in posts DB ### */
-        // // Defining Resources collection
-        // const resources = postsDB.collection("resources");
+        // Correct method to list databases
+        const dbList = await client.db().command({ listDatabases: 1 });
+        console.log('DBs on mongodb://localhost:27017/:', dbList.databases);
 
-        // // adding entries
-        // console.log("\nAdding documents to resource collection...");
-        // await addEntries(resources, resourcePosts);
-        // const resourceColl = await getEntries(resources);
-
-        // // Verify creating resource collection
-        // if (resourceColl.length === resourcePosts.length) {
-        //     console.log("All resource posts were added to \"Resource\" collection");
-        // } else if (resourceColl.length > resourcePosts.length) {
-        //     console.log("There might be dupicate items from `resourcePosts` in \"Resource\" collection");
-        // }  else if (resourceColl.length < resourcePosts.length) {
-        //     console.log("Some items from `resourcePosts` were not added to \"Resource\" collection");
-        // }
-        // 
-        // /* ### Building Advice collection in posts DB ### */
-        // // Defining Advice collection
-        // const advice = postsDB.collection("advice");
-
-        // // adding entries
-        // console.log("\nAdding documents to advice collection...");
-        // await addEntries(advice, advicePosts);
-        // const adviceColl = await getEntries(advice);
-
-        // // Verify creating advice collection
-        // if (adviceColl.length === advicePosts.length) {
-        //     console.log("All advice posts were added to \"Advice\" collection");
-        // } else if (adviceColl.length > advicePosts.length) {
-        //     console.log("There might be dupicate items from `advicePosts` in \"Advice\" collection");
-        // }  else if (adviceColl.length < advicePosts.length) {
-        //     console.log("Some items from `advicePosts` were not added to \"Advice\" collection");
-        // }
-
-        /* ### Building Limbo collection in posts DB ### */
-        // Defining Advice collection
+        // ### Building Limbo collection in posts DB ###
         const limbo = postsDB.collection("limbo");
 
-        // adding entries
+        // Adding entries to the limbo collection
         console.log("\nAdding documents to limbo collection...");
         await addEntries(limbo, pendingPosts);
         const limboColl = await getEntries(limbo);
 
-        // Verify creating advice collection
+        // Verify creating limbo collection
         if (limboColl.length === pendingPosts.length) {
-            console.log("All advice posts were added to \"Advice\" collection");
+            console.log("All pending posts were added to \"Limbo\" collection");
         } else if (limboColl.length > pendingPosts.length) {
-            console.log("There might be dupicate items from `pendingPosts` in \"Advice\" collection");
-        }  else if (limboColl.length < pendingPosts.length) {
-            console.log("Some items from `pendingPosts` were not added to \"Advice\" collection");
+            console.log("There might be duplicate items from `pendingPosts` in \"Limbo\" collection");
+        } else if (limboColl.length < pendingPosts.length) {
+            console.log("Some items from `pendingPosts` were not added to \"Limbo\" collection");
         }
+
+        // Example: Deleting entries from limbo collection with a query
+        console.log("\nDeleting documents from limbo collection where status is 'pending'...");
+        const deleteResult = await deleteEntries(limbo, { status: "pending" });
+        console.log(`${deleteResult.deletedCount} documents were deleted from limbo collection`);
+
+        // Example: Looking for entries in the limbo collection
+        console.log("\nLooking for documents in limbo collection with 'status' as 'approved'...");
+        const lookResult = await lookEntries(limbo, { status: "approved" });
+        console.log(`Found ${lookResult.length} documents with 'approved' status in limbo collection`);
 
     } catch (e) { // If something went wrong, say so
         console.error(e);
@@ -102,6 +112,3 @@ async function configurePostsDB() {
         await client.close();
     }
 }
-
-configurePostsDB().catch(console.error);
-
