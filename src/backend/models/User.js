44ad77a2616cbd645d9fb0758c7f0147446f1889
djseth-81 <sethdjohnson81@gmail.com>
2024-12-05 01:03:46 -0600

    deleted:    src/backend/Yolvin-Unit_Tests/dbFunctions.test.js 	deleted:    src/backend/adviceDB.js 	deleted:    src/backend/createDatabase.js 	modified:   src/backend/models/User.js 	deleted:    src/backend/resourcesDB.js 	modified:   src/backend/server.js

--- src/backend/models/User.js
+++ src/backend/models/User.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  verificationCode: {
-    type: String,
-  },
-  verificationCodeExpiry: {
-    type: Date,
-  },
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-});
+// const userSchema = new mongoose.Schema({
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   verificationCode: {
+//     type: String,
+//   },
+//   verificationCodeExpiry: {
+//     type: Date,
+//   },
+//   isVerified: {
+//     type: Boolean,
+//     default: false,
+//   },
+// });
 
-module.exports = mongoose.model('User', userSchema);
+// module.exports = mongoose.model('User', userSchema);
