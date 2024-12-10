2ec5027cf1b5dcc40bc21658f53c006012b97fec
TooAnonymous <131053442+TooAnonymous@users.noreply.github.com>
2024-12-09 22:15:51 -0600

    modified:   src/backend/testMail.js

--- src/backend/testMail.js
+++ src/backend/testMail.js
@@ -1,18 +1,16 @@
-// Load environment variables from .env file
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-// Import SendGrid's Node.js mail package
 const sgMail = require('@sendgrid/mail')
 
-// Set the SendGrid API key from environment variable
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 // Define the email message
 const msg = {
-  to: 'sjohnson154@unomaha.edu',  // Replace with your recipient email
-  from: 'jumpstart4830@gmail.com',  // Replace with your verified sender email
+  to: 'sjohnson154@unomaha.edu', 
+  from: 'jumpstart4830@gmail.com',  
   subject: 'Verify Your Email Address',
-  text: 'Your verification code is: 123456\n\n\n',  // Plain text message
+  text: 'Your verification code is: 123456\n\n\n', 
 }
 
 // Send the email
